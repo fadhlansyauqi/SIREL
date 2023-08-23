9f33f30d98aae7196352ff0e105d95a296f2a14f
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-23 18:28:57 +0700

    membuat sistem auth- gagal login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    function login(Request $request) 
+    function login() 
     {
         return view('login');
     }
 
-    function register(){
+    function register()
+    {
         return view('register');
+    }
+
+    function authenticating(Request $request)  
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+        
+        // cek apakah login bener
+        
+        if (Auth::attempt($credentials)) {
+            // cek apakah user statusnya active
+            if (Auth::user()->status != 'active') {
+                Session::flash('status', 'failed');
+                Session::flash('message', 'Akun anda belum aktif, silahkan kontak admin!');
+                return redirect('/login');
+            }
+            // $request->session()->regenerate();
+
+            // return redirect()->intended('dashboard');
+        }
+
+        Session::flash('status', 'failed');
+        Session::flash('message', 'Username atau password salah!');
+        return redirect('/login');
     }
 }
