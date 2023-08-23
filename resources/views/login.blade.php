256861a5b2134fdb0a4b88bb002bd463f96258ac
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-23 14:39:02 +0700

    Decorating Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,9 +8,45 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
+<style> 
+    .main {
+        height: 100vh;
+        box-sizing: border-box; 
+    }
+
+    .login-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 30px;
+    }
+
+    form div {
+        margin-bottom: 15px;
+    }
+</style>
 <body>
     
-    Ini adalah Halaman Login
+    <div class="main d-flex justify-content-center align-items-center">
+       <div class="login-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name ="username" id="username" class="form-control">
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name ="password" id="password" class="form-control">
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Login</button>
+                </div>
+                <div class="text-center">
+                    <a href="#">Sign Up</a>
+                </div>
+            </form>
+       </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
