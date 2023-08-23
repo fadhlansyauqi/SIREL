ec080d96fcd6007b1d325706faffbaf548e56972
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-23 15:44:32 +0700

    set middleware

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login']);
+Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::get('register', [AuthController::class, 'register']);
