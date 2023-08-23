d04544eb7de2715a136e39cba4bd07dd6c3fb12e
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-23 23:26:43 +0700

    Groupping middleware

---
+++ app/Http/Controllers/LaptopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaptopController extends Controller
+{
+    function index() 
+    {
+        
+        dd('ini halaman laptop');    
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LaptopController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,9 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('register', [AuthController::class, 'register']);
+Route::middleware('only_guest')->group(function() {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register']);
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth', 'only_admin');
-Route::get('profile', [UserController::class, 'profile'])->middleware('auth', 'only_client');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware('only_admin');
+    Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
+    Route::get('laptops', [LaptopController::class, 'index']);
+});
