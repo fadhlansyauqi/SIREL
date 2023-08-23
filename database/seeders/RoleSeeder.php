b53f78ff694d230467d4bdc65eae89b4218d9c18
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-23 13:44:35 +0700

    Set up login page

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
