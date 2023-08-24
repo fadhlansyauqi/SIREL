80b3a47fbab571154e0cd93be05cc1ebe6507884
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-24 09:45:50 +0700

    decorating register form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,10 +8,51 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
+<style>
+    .main {
+        height: 100vh;
+    }
+    .register-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 30px;
+    }
+    form div {
+        margin-bottom: 15px;
+    }
+</style>
 <body>
     
-    Ini adalah halaman register 
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <div class="register-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name ="username" id="username" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name ="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name ="phone" id="phone" class="form-control" required>
+                </div>
+                <div>
+                    <label for="address" class="form-label">Address</label>
+                    <textarea name="address" id="address" class="form-control" rows="5"></textarea>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Register</button>
+                </div>
+                <div class="text-center">
+                    <a href="login">Login</a>
+                </div>
+            </form>
+        </div>
+    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
