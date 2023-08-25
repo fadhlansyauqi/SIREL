7e6a353dc1abeba77c18686af49f184936c361b6
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-25 22:20:08 +0700

    adding layout template

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     function index() 
     {
-        dd('ini halaman index');    
+        return view('dashboard');  
     }
 }
---
+++ resources/views/layouts/mainlayout.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SIREL | @yield('title')</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    </head>
+    <style>
+        .main {
+            height: 100vh;   
+        }
+  
+        .sidebar {
+            background: rgb(94, 94, 94);
+            color: white;
+        }
+    </style>
+    <body>
+        <div class="main d-flex flex-column justify-content-between">
+            <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+                <div class="container-fluid">
+                    
+                    <a class="navbar-brand" href="#">SIREL</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                </div>
+            </nav>
+
+            <div class="body-content h-100">
+                <div class="row g-0 h-100">
+                    <div class="sidebar col-2">
+                        adadasd
+                    </div>
+                    <div class="content p-5 col-10">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div>
+            @yield('content')
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
