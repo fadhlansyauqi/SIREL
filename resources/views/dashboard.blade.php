3ca548a5f9ea71eef14d22baf80b5f2e5e1a4064
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-26 15:38:07 +0700

    adding content on admin dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,45 @@
 @section('page-name', 'dashboard')
 
 @section('content')
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi dignissimos modi ipsum nemo aut, suscipit veritatis. Accusamus nam molestiae eum totam repellat ducimus quos voluptatem vitae sapiente? Officia ipsum, sapiente atque culpa at inventore excepturi saepe rerum reiciendis, accusantium, sit tempora laborum odio iste error vel nobis consequatur autem nemo? At, laudantium iusto perferendis deserunt non facere excepturi voluptates expedita veritatis, fuga velit labore cumque quo, amet ea repellendus et dolores odit laboriosam accusantium aliquam doloremque. Repellendus molestiae dignissimos mollitia quos tenetur illo omnis suscipit asperiores, natus, saepe, dolores esse error dolorem fuga quidem ex enim a incidunt totam maxime? Commodi quasi accusantium ex, odio porro laborum qui quaerat sunt incidunt in officia impedit corrupti minus vitae mollitia ipsum doloribus, ratione totam. Dolores cupiditate ipsa modi ducimus debitis molestias, cumque quod beatae impedit iste quam voluptas rerum iure aliquid eos quisquam nesciunt officia explicabo aspernatur deleniti adipisci? Natus facilis inventore soluta voluptate dicta? Perspiciatis officia quo consequuntur voluptatem esse ex molestias. Sit ratione quibusdam soluta harum pariatur perspiciatis sequi exercitationem debitis totam libero eveniet similique, ad tempore officia fugiat cupiditate nam, ipsa a deleniti, quae officiis deserunt et hic. Aspernatur deserunt ad atque, nesciunt hic, possimus autem, tempore quidem molestiae vitae rerum maxime quisquam incidunt consequuntur quaerat numquam sequi. Facere, hic itaque ea exercitationem explicabo suscipit qui autem earum quas ut dignissimos, deserunt perspiciatis. Consequuntur, accusamus nemo veniam, eum nulla omnis sed facilis reiciendis similique illo, dolores hic commodi adipisci itaque numquam voluptatem tempora et cum! Consequatur ipsa dicta aliquam magnam commodi eaque sapiente vitae dignissimos molestiae. Maxime eaque ex ad impedit aut ea at quam. Vel, numquam in explicabo praesentium nemo rerum nisi atque fugit aperiam possimus vitae autem officia laboriosam cupiditate modi veniam at pariatur optio, qui labore excepturi ipsum quam. Vitae at, maxime hic illum aliquam aliquid dolor nobis iure eveniet impedit. Hic praesentium eaque ducimus dignissimos incidunt, libero aut deleniti quis magni ex placeat tenetur. Laboriosam, nobis sequi ipsa voluptatem fuga corrupti cupiditate ratione accusamus! Consectetur molestias mollitia temporibus doloremque velit eaque, corrupti maxime minima adipisci voluptatum rem distinctio quo rerum cum quibusdam architecto ab, dolorem modi placeat. Aliquid impedit perspiciatis error iure recusandae voluptatum quasi odio porro? Placeat nisi assumenda error sed ratione doloribus libero neque quae at obcaecati ipsam explicabo qui officiis aliquam, optio accusamus, eveniet rem eos magni. Accusamus sequi quos cum temporibus alias enim ea esse reiciendis consectetur, repellendus eligendi libero quam sed numquam iusto debitis explicabo quas. Quia at eaque dolore asperiores ea amet laudantium illo animi repellendus quibusdam atque eum error itaque, omnis autem fugiat blanditiis quaerat. Veritatis nulla ea iusto est fuga tempore velit magnam ipsa, odio saepe molestiae error ex. Autem ipsam asperiores quae unde impedit rerum sunt et? Molestias pariatur maiores ipsa ducimus, ut voluptas odio! Quidem aliquid eum hic dicta. Cupiditate, sequi, quia illum accusamus beatae natus vitae voluptas tenetur voluptatibus, et necessitatibus quod soluta. Deserunt quasi ab corporis tempora vero consectetur unde alias ea totam ipsam neque rerum libero enim, amet repellendus nesciunt minus tempore!
-    </p>
+
+    <h1>Welcome, {{Auth::user()->username}}</h1>
+
+    <div class="row mt-5">
+        <div class="col-lg-4">
+            <div class="card-data laptop">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-journal-bookmark"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Laptops</div>
+                        <div class="card-count">109</div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <div class="col-lg-4">
+            <div class="card-data category">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-list-task"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Categories</div>
+                        <div class="card-count">27</div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <div class="col-lg-4">
+            <div class="card-data user">
+                <div class="row">
+                    <div class="col-6"><i class="bi bi-people"></i></div>
+                    <div class="col-6 d-flex flex-column justify-content-center align-items-end">
+                        <div class="card-desc">Users</div>
+                        <div class="card-count">150</div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+
 @endsection
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -5,31 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SIREL | @yield('title')</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="{{'css/style.css'}}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     </head>
-    <style>
-        .main {
-            height: 100vh;   
-        }
-  
-        .sidebar {
-            background: rgb(94, 94, 94);
-            color: white;
-        }
-
-        
-
-        .sidebar a {
-            color: #fff;
-            text-decoration: none;
-            display: block;
-            padding: 20px 10px;
-        }
-
-        .sidebar a:hover{
-            background: #000 ;
-        }
-    </style>
     <body>
         <div class="main d-flex flex-column justify-content-between">
             <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
@@ -43,19 +21,20 @@
                 </div>
             </nav>
 
+
             <div class="body-content h-100">
                 <div class="row g-0 h-100">
                     <div class="sidebar col-lg-2 collapse d-lg-block" id="buttonNavbar">
                             @if(Auth::user()->role_id == 1)
-                                <a href="dashboard">Dashboard</a>
-                                <a href="laptops">Laptops</a>
-                                <a href="categories">Categories</a>
-                                <a href="users">Users</a>
-                                <a href="rent-logs">Rent Log</a>
+                                <a href="dashboard" @if(request()->route()->uri == 'dashboard') class='active' @endif>Dashboard</a>
+                                <a href="laptops" @if(request()->route()->uri == 'laptops') class='active' @endif>Laptops</a>
+                                <a href="categories" @if(request()->route()->uri == 'categories') class='active' @endif>Categories</a>
+                                <a href="users"@if(request()->route()->uri == 'users') class='active' @endif>Users</a>
+                                <a href="rent-logs" @if(request()->route()->uri == 'rent-logs') class='active' @endif>Rent Log</a>
                                 <a href="logout">Logout</a>
 
                             @else
-                                <a href="profile">Profile</a>
+                                <a href="profile"@if(request()->route()->uri == 'profile') class='active' @endif>Profile</a>
                                 <a href="logout">Logout</a>
                             @endif
                     </div>
@@ -66,10 +45,7 @@
             </div>
 
         </div>
-
-        <div>
-            @yield('content')
-        </div>
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     </body>
 </html>
