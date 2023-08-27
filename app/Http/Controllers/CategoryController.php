8bc05ff100e69d337ad69761405f18133fd1da42
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-27 10:04:45 +0700

    add update on category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -27,4 +27,22 @@ class CategoryController extends Controller
        $category = Category::create($request->all());
        return redirect('categories')->with('status', 'Kategori Berhasil Ditambahkan!');
     }
+
+    function edit($slug)
+    {
+        $category = Category::where('slug', $slug)->first();
+        return view('category-edit', ['category' => $category]);
+    }
+
+    function update(Request $request, $slug) 
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:categories|max:100',
+        ]); 
+        
+        $category = Category::where('slug', $slug)->first();
+        $category->slug = null;
+        $category->update($request->all());
+        return redirect('categories')->with('status', 'Kategori Berhasil Diperbaharui!');
+    }
 }
