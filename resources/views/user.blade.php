160bbdf64b52938775cc1494df98c855d36620b1
fadhlansyauqi <adhansyauqi@gmail.com>
2023-08-28 00:09:25 +0700

    edit user page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,5 +3,42 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <h1>ini adalah halaman user</h1>
+    <h1>User List</h1>
+
+    <div class="mt-5 d-flex justify-content-end">
+        <a href="#" class="btn btn-secondary me-3">View Banned User</a>
+        <a href="#" class="btn btn-primary">New Registered User</a>
+    </div>
+
+    <div class="my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Username</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $item)
+                    <tr>
+                        <td>{{$loop->iteration}}</td>
+                        <td>{{$item->username}}</td>
+                        <td>
+                            @if ($item->phone)
+                                {{$item->phone}}
+                            @else
+                                -
+                            @endif
+                        </td>
+                        <td>
+                            <a href="#">detail</a>
+                            <a href="#">ban user</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
